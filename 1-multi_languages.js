0a5a6f883d817e33e9bb64814a6627bc4f32ce77
Benedict Ibrahim <benedictibraheem@gmail.com>
2025-08-16 00:49:53 +0100

    Wrote a script that prints 3 lines

---
+++ 1-multi_languages.js
@@ -0,0 +1,7 @@
+const firstScript = "C is Fun";
+const secondScript = "Python is cool";
+const thirdScript = "JavaScript is amazing";
+
+console.log(firstScript);
+console.log(secondScript);
+console.log(thirdScript);
