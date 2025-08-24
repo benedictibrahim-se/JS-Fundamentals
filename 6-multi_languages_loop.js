2eb14e92de2a01c2b6b46d944d6896d0ce665dd6
Benedict Ibrahim <benedictibraheem@gmail.com>
2025-08-24 21:05:11 +0100

    Wrote a script that prints 3 lines: but by using an array of string and a loop

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+const multiLanguages = [
+    "C is Fun", 
+    "Python is Cool", 
+    "JavaScript is Amazing"
+];
+
+for (let i = 0; i < multiLanguages.length; i++) {
+    console.log(multiLanguages[i]);
+}
